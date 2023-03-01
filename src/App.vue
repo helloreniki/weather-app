a1272b43783282426cafc762335bdfef594948f3
Reniki <hello@reniki.com>
2023-03-01 20:17:03 +0100

    catch error

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,14 @@
         <!-- result card -->
         <div v-if="weather" class="my-20 flex flex-col gap-2 p-8 w-fit bg-gray-50 rounded-lg shadow-lg">
             <h2 class="text-2xl font-semibold border-b-2 border-gray-500 mb-2">{{  weather.city }}</h2>
-            <p>{{ weather.description[0].description }}</p>
+            <p>{{ weather.description?.description }}</p>
             <p>Temperature: {{ weather.main.temp }} st C</p>
             <p>Feels like: {{ weather.main.feels_like }} st C</p>
             <p>T min: {{ weather.main.temp_min }} st C</p>
             <p>T max: {{ weather.main.temp_max }} st C</p>
             <p>Huimdity: {{ weather.main.humidity }} st C</p>
         </div>
+        <div v-if="err" class="text-red-500 mt-3 pl-4">{{ err }}</div>
 
     </div>
 </template>
@@ -31,20 +32,31 @@ const apiKey = import.meta.env.VITE_API_KEY
 async function getWeather(){
     try {
         await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${apiKey}`)
-                    .then(response => response.json())
+                    .then(response => {
+                        console.log(response)
+                        if(response.ok){
+                           return response.json()
+                        } else {
+                            throw new Error(response.statusText)
+                        }
+                    })
                     // .then(data => weather.value = data.main)
                     .then(data => {
-                        // console.log(data)
-                       weather.value = []
-                       weather.value['city'] = data.name
-                       weather.value['main'] = data.main
-                       weather.value['description'] = data.weather
+                       console.log(data)
+                       err.value = null
+                       if(data) {
+                           weather.value = []
+                           weather.value['city'] = data.name
+                           weather.value['main'] = data.main
+                           weather.value['description'] = data.weather[0]
+                       }
                     })
 
-        console.log(weather.value)
+        console.log('weather', weather.value)
+
 
     } catch (error) {
-        err.value = error
+        err.value = error.message
     }
 
 }
