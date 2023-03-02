83f829d185d2767f40058507f9ae3c500ab3e9ce
Reniki <hello@reniki.com>
2023-03-02 19:16:12 +0100

    show history searches

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,24 @@
         </div>
         <div v-if="err" class="text-red-500 mt-3 pl-4">{{ err }}</div>
 
+        <!-- history -->
+        <div v-if="historyResults.length > 0">
+            <h3 class="mb-4 font-semibold text-xl">Recent Searches</h3>
+            <div  v-for="city in historyResults" :key="city" class="flex gap-4 items-center">
+                <h3 class="font-semibold border rounded-lg px-4 py-2 shadow-md" @click="getCityWeather(city)">{{ city }}</h3>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const result = ref(null)
 const search = ref('')
 const err = ref(null)
+const historyResults = ref([])
 
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 const apiKey = import.meta.env.VITE_API_KEY
@@ -45,14 +54,23 @@ async function getWeather(){
                     //    console.log('name', name)
                     //    console.log('main', main)
                     //    console.log('weather', weather)
-                       err.value = null
-                       result.value = main
-                       result.value['name'] = name
-                       result.value['description'] = weather[0].description
-                       result.value['description_short'] = weather[0].main
+                        err.value = null
+                        result.value = main
+                        result.value['name'] = name
+                        result.value['description'] = weather[0].description
+                        result.value['description_short'] = weather[0].main
+                    //    console.log('result name', result.value.name)
+                    //    console.log(historyResults)
+
+                        historyResults.value.unshift(result.value.name)
+                        if(historyResults.value.length > 5){
+                            // only get first 5
+                            historyResults.value = historyResults.value.slice(0, 5)
+                        }
                     })
 
-        console.log('result', result.value)
+
+        console.log('history', historyResults.value)
 
 
     } catch (error) {
@@ -61,8 +79,17 @@ async function getWeather(){
 
 }
 
+console.log(historyResults)
 
+function getCityWeather(city) {
+    search.value = city
+    getWeather()
+}
 
+// add debounce, dont want for every key stroke
+// watch(search, () => {
+//     getWeather()
+// } )
 
 
 </script>
